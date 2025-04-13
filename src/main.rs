76aba2d3c6f79e8819c7ce0414be1a4c6e2768df
Mathias Stadler <stadler-mathias@web.de>
2025-04-13 20:03:01 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    //TODO do something
+    //FIXME fix follow line
+
+    /*hallo
+    
+    dudoof
+    
+    das ist nicht
+    
+    richtig
+     */
     
 }
